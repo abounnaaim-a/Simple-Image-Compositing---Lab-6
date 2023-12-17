6dad775607205401b61a450d18c223cd520b0cb1
abounnaaim-a <aabounna@purdue.edu>
2023-12-16 20:43:55 -0500

    CGT 215

--- Simple Image Compositing - Lab 6.cpp
+++ Simple Image Compositing - Lab 6.cpp
@@ -2,10 +2,52 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace sf;
+using namespace std;
+
+int main() {
+    string background = "winter.png";
+    string foreground = "yoda.png";
+    Texture backgroundTex;
+    if (!backgroundTex.loadFromFile(background)) {
+        cout << "Couldn't Load Image" << endl;
+        exit(1);
+    }
+    Texture foregroundTex;
+    if (!foregroundTex.loadFromFile(foreground)) {
+        cout << "Couldn't Load Image" << endl;
+        exit(1);
+    }
+    Image backgroundImage;
+    backgroundImage = backgroundTex.copyToImage();
+    Image foregroundImage;
+    foregroundImage = foregroundTex.copyToImage();
+
+    Vector2u sz = backgroundImage.getSize();
+    Color greenScreenColor = foregroundImage.getPixel(0, 0);
+
+    for (int y = 0; y < sz.y; y++) {
+        for (int x = 0; x < sz.x; x++) {
+            Color foregroundColor = foregroundImage.getPixel(x, y);
+            if (foregroundColor == greenScreenColor) {
+                Color backgroundColor = backgroundImage.getPixel(x, y);
+                foregroundImage.setPixel(x, y, backgroundColor);
+
+            }
+        }
+    }
+    RenderWindow window(VideoMode(1024, 768), "Here's the output");
+    Sprite sprite1;
+    Texture tex1;
+    tex1.loadFromImage(foregroundImage);
+    sprite1.setTexture(tex1);
+    window.clear();
+    window.draw(sprite1);
+    window.display();
+    while (true);
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
